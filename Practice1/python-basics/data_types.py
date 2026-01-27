bfc45458d82cb3136a535b4b8b0b18757e450dd6
nytrale <alim.ospanov06@gmail.com>
2026-01-27 13:31:53 +0500

    added comments

--- Practice1/python-basics/data_types.py
+++ Practice1/python-basics/data_types.py
@@ -1,12 +1,12 @@
 x = 5
 print(type(x))
 
-# x = "Hello World"	#str	
-# x = 20	#int	
-# x = 20.5	#float	
-# x = 1j	#complex	
-# x = ["apple", "banana", "cherry"]	#list	
-# x = ("apple", "banana", "cherry")	#tuple	
-# x = range(6)	#range	
-# x = {"name" : "John", "age" : 36}	#dict	
-# x = {"apple", "banana", "cherry"}	#set
+x = "Hello World"	#str	
+x = 20	#int	
+x = 20.5	#float	
+x = 1j	#complex	
+x = ["apple", "banana", "cherry"]	#list	
+x = ("apple", "banana", "cherry")	#tuple	
+x = range(6)	#range	
+x = {"name" : "John", "age" : 36}	#dict	
+x = {"apple", "banana", "cherry"}	#set
