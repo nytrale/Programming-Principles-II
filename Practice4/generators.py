bb4c0547b1f04e8508bb7b31bfa698a62342c97c
nytrale <alim.ospanov06@gmail.com>
2026-02-24 13:09:23 +0500

    edited

--- Practice4/generators.py
+++ Practice4/generators.py
@@ -1,17 +1,13 @@
 # 1) Create a generator that generates the squares of numbers up to some number N.
 def squares_up_to(N):
-    # yield squares from 0^2 to N^2
     for x in range(0, N + 1):
         yield x * x
 
-# Example:
-# for v in squares_up_to(5):
-#     print(v)
+#pause and resume  execution.
 
 
 # 2) Print even numbers between 0 and n in comma separated form (using generator)
 def evens_up_to(n):
-    # yield even numbers from 0 to n
     for x in range(0, n + 1):
         if x % 2 == 0:
             yield x
@@ -23,12 +19,10 @@ print(",".join(str(x) for x in evens_up_to(n)))
 
 # 3) Generator: numbers divisible by 3 and 4 between 0 and n
 def divisible_by_3_and_4(n):
-    # divisible by 3 and 4 means divisible by 12
     for x in range(0, n + 1):
         if x % 3 == 0 and x % 4 == 0:
             yield x
 
-# Example:
 # for v in divisible_by_3_and_4(100):
 #     print(v)
 
@@ -39,17 +33,15 @@ def squares(a, b):
     for x in range(a, b + 1):
         yield x * x
 
-# Test with a for loop:
 # for v in squares(3, 7):
 #     print(v)
 
 
 # 5) Generator that returns all numbers from n down to 0
 def countdown(n):
-    # yield n, n-1, ..., 0
     for x in range(n, -1, -1):
         yield x
 
-# Example:
+   # yield n, n-1, ..., 0
 # for v in countdown(5):
 #     print(v)
